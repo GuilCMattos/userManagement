bf14cdb5b52a06e95765d083b916cad1bd0956ce
Guilherme <mttsguil@gmail.com>
2022-08-22 12:06:54 -0300

    add: required with javascript in form

--- controller/UserController.js
+++ controller/UserController.js
@@ -81,8 +81,15 @@ class UserController {
     getValues() { 
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach((field, index) => { 
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value ) {
+
+                field.parentElement.classList.add('has-error')
+                isValid = false;
+            }
 
             if(field.name == "gender") { 
     
@@ -100,6 +107,10 @@ class UserController {
             }
             
         });
+
+        if (!isValid) { 
+            return false
+        }
         
         return new User(
             user.name,
