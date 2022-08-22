1a90dd8e80c04b541877a66edb3fba9dfb6158de
Guilherme <mttsguil@gmail.com>
2022-08-22 11:14:09 -0300

    refac: add ternary operation in user admin

--- controller/UserController.js
+++ controller/UserController.js
@@ -57,7 +57,13 @@ class UserController {
             reject(e)
         }
 
+           if(file) {
+
             fileReader.readAsDataURL(file);
+
+           } else { 
+            resolve('dist/img/boxed-bg.jpg');
+           }
 
         })
 
@@ -77,6 +83,10 @@ class UserController {
                     user[field.name] = field.value    
                 }
         
+            } else if(field.name == "admin") { 
+
+                user[field.name] = field.checked
+
             } else { 
                 user[field.name] = field.value;
             }
@@ -102,7 +112,7 @@ class UserController {
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${dataUser.admin ? "Sim" : "Não"}</td>
         <td>${dataUser.birth}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
